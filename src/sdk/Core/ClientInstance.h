621547ae580123df9990c16cb32f014e1d66f70b
chyves <82107846+notchyves@users.noreply.github.com>
2024-02-12 18:09:34 -0500

    this is what we call real gang sdk
    thug 4 life

--- src/sdk/Core/ClientInstance.h
+++ src/sdk/Core/ClientInstance.h
@@ -1,6 +1,13 @@
 #pragma once
 
-class ClientInstace {
+#include <stdint.h>
+#include "../../utils/mem.h"
+#include "../Entity/LocalPlayer.h"
+
+class ClientInstance {
 public:
-    
+    LocalPlayer* getLocalPlayer()
+    {
+        return CallVFunc<27, LocalPlayer*>(this);
+    }
 };
